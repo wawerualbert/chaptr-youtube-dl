71e8ace558bacf61878221d54f02d094dff34959
wawerualbert <50580926+wawerualbert@users.noreply.github.com>
2021-10-23 07:47:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -63,26 +63,23 @@ master.title("Chaptr Youtube Downloader")
 
 tk.Label(master,text="Enter URL Here").grid(row=0)
 
-
 e1 = tk.Entry(master , textvariable='Ctrl + V')
 e1.grid(row=0, column=1)
 
 tk.Button(master, text='Download Video', command=download_video).grid(row=3, column=1,sticky=tk.W,pady=4)
 tk.Button(master, text='Select Destination', command=destination).grid(row=0, column=2,sticky=tk.W,pady=4)
-destination_label = ttk.Label(master, text= "Chaptr Youtube-dl" , foreground = 'red' , background = 'white' ,font = ('Times New Roman' , 16 , 'bold'))
+destination_label = ttk.Label(master, text= "Chaptr Youtube-dl" , foreground = 'green' , background = 'white' ,font = ('Times New Roman' , 16 , 'bold'))
 destination_label.grid(column = 6, row = 0 , sticky=tk.W,pady=4)
 
-
 var = IntVar()
-R1 = Radiobutton(master, text="Download Full Playlist", variable=var, value=1,
+download_button = Radiobutton(master, text="Download Full Playlist", variable=var, value=1,
                   command=sel).grid(row= 4 , column = 0)
 
-R2 = Radiobutton(master, text="Range of Videos", variable=var, value=2,
+download_playlist_button = Radiobutton(master, text="Range of Videos", variable=var, value=2,
                   command=sel).grid(row= 5 , column = 0)
 ran = tk.Entry(master , width=10)
 
 ran.grid(row=5, column=1)
-
 
 label = Label(master)
 label.grid(row= 7 , column = 0)
